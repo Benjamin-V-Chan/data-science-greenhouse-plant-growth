19e59b8a2c64a42be421c0ddfaff829ed761afde
BenjaminChan <benjamin.v.chan@gmail.com>
2025-05-09 20:03:09 -0700

    07_visualize script complete

--- scripts/07_visualize.py
+++ scripts/07_visualize.py
@@ -1,5 +1,27 @@
-# import joblib, matplotlib.pyplot, numpy, os
-# load best model (e.g., RandomForest), pca, X_test_pca, y_test, feature names
-# if RandomForest: importances = model.feature_importances_; sorted_indices = np.argsort(importances)[::-1]
-# plot bar of top 10 importances; save 'outputs/07_feature_importance.png'
-# plot PCA scatter: plt.scatter(X_test_pca[:,0], X_test_pca[:,1], c=label_encoding(y_test)); save 'outputs/07_pca_scatter.png'
+import os
+import joblib
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    os.makedirs('outputs', exist_ok=True)
+    X_train_pca, X_test_pca, y_train, y_test = joblib.load('outputs/engineered_data.joblib')
+    model = joblib.load('outputs/05_rf_model.joblib')
+    importances = model.feature_importances_
+    idxs = np.argsort(importances)[::-1][:10]
+    plt.bar(range(10), importances[idxs])
+    plt.xticks(range(10), idxs)
+    plt.ylabel('Importance')
+    plt.title('Top 10 Feature Importances')
+    plt.savefig('outputs/07_feature_importance.png')
+    plt.clf()
+    # PCA scatter on first two components
+    plt.scatter(X_test_pca[:,0], X_test_pca[:,1], c=y_test.map({c:i for i,c in enumerate(sorted(set(y_test)))}))
+    plt.xlabel('PC1')
+    plt.ylabel('PC2')
+    plt.title('PCA Scatter by Class')
+    plt.savefig('outputs/07_pca_scatter.png')
+    plt.clf()
+
+if __name__ == "__main__":
+    main()
