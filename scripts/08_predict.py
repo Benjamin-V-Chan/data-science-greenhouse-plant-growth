d8c9c952da4f8ec527c1dff8b4332192c0234b3c
BenjaminChan <benjamin.v.chan@gmail.com>
2025-05-09 20:03:29 -0700

    08_predict script complete

--- scripts/08_predict.py
+++ scripts/08_predict.py
@@ -1,10 +1,22 @@
-# import pandas, joblib, argparse, os
-# parser = argparse.ArgumentParser(); parser.add_argument('--input', ...); parser.add_argument('--output', ...)
-# args = parser.parse_args()
-# load preprocessor, pca, model from 'outputs'
-# df_new = pd.read_csv(args.input)
-# X_new = preprocessor.transform(df_new.drop(columns=['Class'], errors='ignore'))
-# X_new_pca = pca.transform(X_new)
-# preds = model.predict(X_new_pca)
-# df_new['Prediction'] = preds
-# df_new.to_csv(args.output, index=False)
+import os
+import argparse
+import pandas as pd
+import joblib
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', required=True)
+    parser.add_argument('--output', required=True)
+    args = parser.parse_args()
+    preprocessor = joblib.load('outputs/preprocessor.joblib')
+    pca = joblib.load('outputs/pca.joblib')
+    model = joblib.load('outputs/05_rf_model.joblib')
+    df = pd.read_csv(args.input)
+    X = df.drop(columns=['Class'], errors='ignore')
+    X_proc = preprocessor.transform(X)
+    X_pca = pca.transform(X_proc)
+    df['Prediction'] = model.predict(X_pca)
+    df.to_csv(args.output, index=False)
+
+if __name__ == "__main__":
+    main()
